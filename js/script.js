b586621ef5784fe4c338ae83384a7cc04140a546
Tom MacWright <macwright@gmail.com>
2011-02-07 17:54:34 -0500

    removing more facebox stuff

--- js/script.js
+++ js/script.js
@@ -6,11 +6,4 @@ $(function() {
         text = (new Showdown.converter()).makeHtml(text);
         $(this).html(text);
     });
-
-    // Set facebox paths.
-    $.facebox.settings.closeImage = 'facebox/src/closelabel.png'
-    $.facebox.settings.loadingImage = 'facebox/src/loading.gif'
-
-    $('.screenshots a').facebox();
 });
-
